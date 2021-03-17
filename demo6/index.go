d665a3d760df5f8ded86b95a7e6ebe9da29072eb
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 13:30:45 +0700

    1234

--- demo6/index.go
+++ demo6/index.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	_ = r
 
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Ok")
+	})
+
+	r.Run(":8081")
 }
