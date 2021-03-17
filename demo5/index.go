1ede21a5ce57869d1e0f3fbffded9c106ef6ec50
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 13:08:44 +0700

    1234

---
+++ demo5/index.go
@@ -0,0 +1,24 @@
+package main
+import (
+    "fmt"
+Simple Http Server
+    "html"
+    "log"
+    "net/http"
+)
+func main() {
+    // Define path "/"
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Home, %q", html.EscapeString(r.URL.Path))
+    })
+    // Define path "/profile"
+    http.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Profile, %q", html.EscapeString(r.URL.Path))
+    })
+    // Define path "/login" and get string query
+    http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Login, %s, %s", r.URL.Query().Get("username"), r.URL.Query().Get("password"))
+    })
+    // Run and show fatal log
+    log.Fatal(http.ListenAndServe(":82", nil))
+}
