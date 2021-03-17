893eb984fc48dfa4f81d7f3b6b86769c31d78aa3
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 11:00:49 +0700

    1234

--- demo2/index.go
+++ demo2/index.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type user struct {
+	username string
+	password string
+}
+
+// product {name, price, qty}
+
 func main() {
 	fmt.Printf("Helloworld")
+
+	u := user{username: "admin", password: "1234"}
+	_ = u
+	fmt.Println(u)
+	fmt.Println(u.username)
 }
