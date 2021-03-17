7a760dd60c88dd2c18c4e89461c8b6155059ce3a
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 11:31:24 +0700

    1234

---
+++ demo3/index.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Hey\n")
+	tmp0 := "Hey1234\n"
+	clear(&tmp0)
+	fmt.Printf(tmp0)
+}
+
+func clear(obj *string) {
+	*obj = "Clear"
+}
