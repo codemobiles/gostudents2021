e2b96aa4e52c7f01e73d01851801aea6c08c74a0
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 10:05:51 +0700

    1234

--- demo1/index.go
+++ demo1/index.go
@@ -8,8 +8,20 @@ func main() {
 	var tmp2 int = 0
 	tmp3 := 1 // Implicit Declaration
 	tmp4 := "Hey"
+	tmp4 = "CodeMobiles"
+
+	const tmp5 string = "lek"
+	// tmp5 = "home"
+
+	var tmp6 []int = []int{1, 2, 3, 4}
+	var tmp7 []string = []string{"Angular", "React", "Golang"}
+
 	fmt.Print(tmp1, "\n")
 	fmt.Printf("Tmp2 is %d\n", tmp2)
 	fmt.Printf("Tmp3 is %d\n", tmp3)
 	fmt.Printf("Tmp4 is %s\n", tmp4)
+	fmt.Printf("Tmp5 is %s\n", tmp5)
+	fmt.Println("Lek")
+	fmt.Printf("ค่ามันคือ %v\n", tmp6)
+	fmt.Printf("หลักสูตรทั้งหมด %v\n", tmp7)
 }
