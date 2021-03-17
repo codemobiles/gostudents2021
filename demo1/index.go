477b28a14f2d1a64e4fe3611338bc43829ed6743
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 09:58:25 +0700

    1234

--- demo1/index.go
+++ demo1/index.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Helloworld2\n")
+	fmt.Print("Helloworld\n")
+	var tmp1 string = "Hello" // Explicit Declaration
+	var tmp2 int = 0
+	tmp3 := 1 // Implicit Declaration
+	tmp4 := "Hey"
+	fmt.Print(tmp1, "\n")
+	fmt.Printf("Tmp2 is %d\n", tmp2)
+	fmt.Printf("Tmp3 is %d\n", tmp3)
+	fmt.Printf("Tmp4 is %s\n", tmp4)
 }
