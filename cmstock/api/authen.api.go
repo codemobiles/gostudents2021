9b23da1248e1cdb0d1bcb8e910def0320d6f2123
codemobiles <chaiyasit.t@gmail.com>
2021-03-17 14:24:16 +0700

    1234

--- cmstock/api/authen.api.go
+++ cmstock/api/authen.api.go
@@ -2,10 +2,15 @@ package api
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupAuthenAPI(router *gin.Engine) {
 	fmt.Println("SetupAuthenAPI is ready")
+	authenAPI := router.Group("/api/v2")
+	authenAPI.GET("/login", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"result": "login"})
+	})
 }
