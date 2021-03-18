96f942ac6fda3842ae045bcb3ed13557ec42a8cb
codemobiles <chaiyasit.t@gmail.com>
2021-03-18 11:05:00 +0700

    1234

---
+++ cmstock/interceptor/jwt.interceptor.go
@@ -0,0 +1,58 @@
+package interceptor
+
+import (
+	"cmstock/model"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secretKey = "87654321"
+
+func JwtSign(payload model.User) string {
+	atClaims := jwt.MapClaims{}
+
+	// Payload begin
+	atClaims["id"] = payload.ID
+	atClaims["username"] = payload.Username
+	atClaims["level"] = payload.Level
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	// Payload end
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, _ := at.SignedString([]byte(secretKey))
+	return token
+
+}
+
+func JwtVerify(c *gin.Context) {
+	tokenString := strings.Split(c.Request.Header["Authorization"][0], " ")[1]
+	fmt.Println(tokenString)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims)
+
+		staffID := fmt.Sprintf("%v", claims["id"])
+		username := fmt.Sprintf("%v", claims["jwt_username"])
+		level := fmt.Sprintf("%v", claims["jwt_level"])
+		c.Set("jwt_staff_id", staffID)
+		c.Set("jwt_username", username)
+		c.Set("jwt_level", level)
+
+		c.Next()
+	} else {
+		c.JSON(http.StatusOK, gin.H{"result": "nok", "message": "invalid token", "error": err})
+		c.Abort()
+	}
+}
