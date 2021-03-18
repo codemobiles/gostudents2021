7c7a314318121ac9b32b805a0e0ebcb5e04daf47
codemobiles <chaiyasit.t@gmail.com>
2021-03-18 15:12:43 +0700

    1234

--- cmstock/react/my-app/src/App.js
+++ cmstock/react/my-app/src/App.js
@@ -3,18 +3,20 @@ import Axios from 'axios'
 
 export default function App() {
 
+  const [youtubes, setYoutubes] = React.useState([])
 
   React.useEffect(async () => {
     const url = "https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=foods"
     const result = await Axios.get(url)
-    console.log(result.data)
+    console.log(result.data.youtubes[0].title)
+    setYoutubes(result.data.youtubes)
   }, [])
 
   return (
     <div>
       <h1>CodeMobiles</h1>
       <ul>
-        {[1,2,3,4].map(item=><li key={item.toString()}>{item}</li>)}         
+        {youtubes.map(item=><li key={item.id}>{item.title}</li>)}         
       </ul>
     </div>
   )
