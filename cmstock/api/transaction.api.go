86dc33629af1a22178fca649c55ea9ab7efd66d9
codemobiles <chaiyasit.t@gmail.com>
2021-03-18 14:24:25 +0700

    1234

--- cmstock/api/transaction.api.go
+++ cmstock/api/transaction.api.go
@@ -1,16 +1,42 @@
 package api
 
 import (
-	"fmt"
+	"main/db"
+	"main/interceptor"
+	"main/model"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupTransactionAPI(router *gin.Engine) {
-	fmt.Println("SetupTransactionAPI is ready")
 	transactionAPI := router.Group("/api/v2")
-	transactionAPI.GET("/transaction", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"result": "transaction"})
-	})
+	{
+		transactionAPI.GET("/transaction" /* interceptor.JwtVerify,*/, getTransaction)
+		transactionAPI.POST("/transaction", interceptor.JwtVerify, createTransaction)
+	}
+}
+
+type TransactionResult struct {
+	Change        float64 `json:"change"`
+	CreatedAt     string  `json:"created_at"`
+	ID            uint    `json:"id"`
+	OrderList     string  `json:"order_list"`
+	Paid          float64 `json:"paid"`
+	PaymentDetail string  `json:"payment_detail"`
+	PaymentType   string  `json:"payment_type"`
+	Total         float64 `json:"total"`
+	Staff         string  `json:"staff_id"`
+}
+
+func getTransaction(c *gin.Context) {
+	// var result []map[string]interface{}
+	var result []TransactionResult
+	db.GetDB().Debug().Raw("SELECT transactions.id, total, paid, change, payment_type, payment_detail, order_list, users.username as Staff, transactions.created_at FROM transactions join users on transactions.staff_id = users.id order by transactions.created_at DESC", nil).Scan(&result)
+	c.JSON(200, result)
+}
+
+func createTransaction(c *gin.Context) {
+	
 }
