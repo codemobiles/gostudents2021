655d860207a6b2c97274f7a3720c5d9b4e87b983
codemobiles <chaiyasit.t@gmail.com>
2021-03-19 09:42:45 +0700

    1234

---
+++ cmstock/webserver/build_cmstock/precache-manifest.fe0568c6c6955dd9a4e6831962f2a099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a36c98e0f7c458c1a8ba46f706b233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0bcbacec9867b33e7e4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8716ba2c072a6191eed",
+    "url": "/static/js/2.cb6bd06f.chunk.js"
+  },
+  {
+    "revision": "fdda530c85a0e9d05f5440b63cd8beb4",
+    "url": "/static/js/2.cb6bd06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bcbacec9867b33e7e4",
+    "url": "/static/js/main.634f68b4.chunk.js"
+  },
+  {
+    "revision": "dc17b17985cc996bc266",
+    "url": "/static/js/runtime-main.422211c3.js"
+  }
+]);
