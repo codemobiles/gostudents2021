eb80b9e8c161fe0c188a7f18c3c03da552b8e49c
codemobiles <chaiyasit.t@gmail.com>
2021-03-19 09:34:18 +0700

    1234

--- cmstock/readme.txt
+++ cmstock/readme.txt
@@ -6,6 +6,9 @@ npx create-react-app my-app
 cd my-app
 npm start
 
+# build or deploy react
+npm run build
+
 # Docker instruction
 docker build -t cmstock:1.0 .       
 docker run -it -p  8081:8081 cmstock:1.0
--- cmstock/webserver/webserver.go
+++ cmstock/webserver/webserver.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static")) // New code
+	fileServer := http.FileServer(http.Dir("./build")) // New code
 	http.Handle("/", fileServer)                        // New code
 
 	fmt.Printf("Starting server at port 5001\n")
